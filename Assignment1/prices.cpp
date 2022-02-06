12d060f9deee28be500494eda9b0965e230cc716
laurenszlosek <szlosek@chapman.edu>
2022-02-06 19:52:42 +0000

    prices.cpp

---
+++ Assignment1/prices.cpp
@@ -0,0 +1,24 @@
+/*
+@file prices.cpp
+@brief C++ program to
+@author Lauren Szlosek
+*/
+#include <iostream>
+#include <string>
+
+int main(){
+  float uPrice;
+  float total;
+
+  while (uPrice >= 0){
+    std::cout << "Enter price: " << std::endl;
+    std::cin >> uPrice;
+    if (uPrice < 0){
+      break;
+    }
+    total += uPrice;
+  }
+
+  std::cout << "Total: $" << total << std::endl;
+  return 0;
+}
